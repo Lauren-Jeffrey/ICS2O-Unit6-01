9eabccc88fdb22d32ecac83163d069d1ed43dfc9
LaurenJeffrey <lauren.jeffrey@ocsbstudent.ca>
2020-12-18 15:56:09 +0000

    js and html edited

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const randomnumber = Math.floor(Math.random() * 6) + 1;
+const randomnumber = Math.floor(Math.random() * 6) + 1
 let myNumber = 0
 // myNumber = parseInt(myNumber)
-function myFunction() {
-  myNumber = document.getElementById('myText').value;
-if (randomnumber == parseInt(myNumber))
-alert('Correct!');
-else alert ('Incorrect, try again.')
+function myFunction () {
+  myNumber = document.getElementById('my-text').value
+  if (randomnumber === parseInt(myNumber))
+    alert('Correct!')
+    else alert('Incorrect, try again.')
 }
