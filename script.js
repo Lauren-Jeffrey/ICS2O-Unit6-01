ccca49e5baf8266cb2b846e0b775fde8365c5c5c
LaurenJeffrey <lauren.jeffrey@ocsbstudent.ca>
2020-12-18 15:36:38 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+let randomnumber = Math.floor(Math.random() * 6) + 1;
+let myNumber = 0;
+//myNumber = parseInt(myNumber);
+function myFunction() {
+  myNumber = document.getElementById('myText').value;
+if (randomnumber == parseInt(myNumber))
+alert('Correct!');
+}
