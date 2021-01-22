50fd38d0cd61f126e7b0939eae514fdb019c9e2f
LaurenJeffrey <lauren.jeffrey@ocsbstudent.ca>
2021-01-22 13:57:11 +0000

    html and js edited

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const randomnumber = Math.floor(Math.random() * 6) + 1
 let myNumber = 0
 // myNumber = parseInt(myNumber)
-document.getElementById('myBtn').addEventListener('click', displayDate)
+document.getElementById('check').addEventListener('click', displayDate)
 function displayDate () {
   myNumber = document.getElementById('my-text').value
   if (randomnumber === parseInt(myNumber)) {
     alert('Correct!')
   }
-  else alert('Incorrect, try again.')
+  else {alert('Incorrect, try again.')
+  }
 }
